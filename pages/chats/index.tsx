8d129cf19501044bbf7743dd5fd9b3ef1e623079
Kyoungwon kang <kkwondev@gmail.com>
2022-09-14 13:00:03 +0900

    #5.17~5.18 Layout

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,21 +1,24 @@
 import { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="py-10 divide-y-[1px]">
-      {[...Array(10)].map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4 cursor-pointer items-center space-x-3 py-3"
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className=" text-gray-700">Steve Jebs</p>
-            <p className="text-sm font-medium text-gray-500">내일봐!!</p>
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px]">
+        {[...Array(10)].map((_, i) => (
+          <div
+            key={i}
+            className="flex px-4 cursor-pointer items-center space-x-3 py-3"
+          >
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className=" text-gray-700">Steve Jebs</p>
+              <p className="text-sm font-medium text-gray-500">내일봐!!</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
