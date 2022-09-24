272bb4a4c7d61dc3b74d63e642ccbfd4b7d68e18
kkwondev <kkwoncokr@gmail.com>
2022-09-24 18:41:04 +0900

    #9.7

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,4 +1,4 @@
-import twilio from "twilio";
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
@@ -7,10 +7,25 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  console.log(req.session);
+
   const { token } = req.body;
   console.log(token);
-
-  return res.status(200).end();
+  const exists = await client.token.findUnique({
+    where: { payload: token },
+    include: { user: true },
+  });
+  if (!exists) res.status(404).end();
+  console.log(exists);
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
+  res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "321893028190381209383219830921839201839012829013920183092830921830921",
+});
